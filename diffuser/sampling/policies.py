0d4518f3bc0283f496f8e368342394c004a46561
augusto <augustotaglemontes@gmail.com>
2024-08-21 22:59:47 -0400

    refactored policy

--- diffuser/sampling/policies.py
+++ diffuser/sampling/policies.py
@@ -7,33 +7,71 @@ import diffuser.utils as utils
 from diffuser.datasets.preprocessing import get_policy_preprocess_fn  # que es esta wea
 
 
-Trajectories = namedtuple('Trajectories', 'actions observations values')
+Trajectories = namedtuple('Trajectories', 'actions observations rewards')
 
-def sort_by_values(trajectories): # refactorizar funcion
+
+def compute_reward_to_go_batch(rewards_batch, gamma):
     """
-    [B,H,(A+S+R)]
+    Compute the reward-to-go for a batch of reward sequences with a discount factor gamma.
+    
+    Parameters:
+        rewards_batch (torch.Tensor): A 2D tensor of shape (B, Horizon,1) where B is the batch size and Horizon is the length of each sequence.
+        gamma (float): The discount factor.
+    
+    Returns:
+        torch.Tensor: A 1D tensor of shape (B) containing reward-to-go values for each sequence in the batch.
     """
-    rewards=trajectories[:,:,-1] # (B,H,1)
+
+    assert rewards_batch.shape[2]==1
+
+    rewards_batch=rewards_batch.squeeze(-1) # (B,H,1) -> (B,H)
+    B, H = rewards_batch.shape
+
+    gamma_tensor = torch.pow(gamma, torch.arange(H, dtype=torch.float32)).to(rewards_batch.device)
+    gamma_matrix = gamma_tensor.unsqueeze(0).repeat(B, 1) # (B,H)
+
+    # Apply gamma matrix to compute reward-to-go
+    reward_to_go_batch = torch.sum(rewards_batch * gamma_matrix, dim=1)  # (B, H) -> (B)
+    
+    return reward_to_go_batch
+
+
+def sort_by_values(actions, observations, rewards, no_inpaint_step,gamma): # refactorizar funcion
+    """
+    [B,past_H+future_H,(A+S+R)]
+    """
+    rewards_to_go=rewards[:,:no_inpaint_step,:]  # notar que aca no son values (B,future_H,1)
+
+    values=compute_reward_to_go_batch(rewards_to_go,gamma) # (B,H,1)-> (B)
 
     # multiplicar por gamma 
     # sumar
 
     inds = torch.argsort(values, descending=True)
-    trajectories = trajectories[inds]
+
+    actions_sorted = actions[inds]
+    observations_sorted=observations[inds]
+    rewards_sorted=rewards[inds]
     values = values[inds]
-    return trajectories, values
+
+    return actions_sorted,observations_sorted,rewards_sorted, values
+
+
 
 class Policy:
 
-    def __init__(self, diffusion_model, dataset, preprocess_fns, **sample_kwargs):
-        #self.guide = guide
+    def __init__(self, diffusion_model, dataset, preprocess_fns,gamma, **sample_kwargs):
         self.diffusion_model = diffusion_model
         self.dataset = dataset # dataset is a instance of the class sequence dataset
         self.action_dim = diffusion_model.action_dim
         self.preprocess_fn = get_policy_preprocess_fn(preprocess_fns)
+        self.gamma=gamma
         self.sample_kwargs = sample_kwargs
 
     def __call__(self, conditions, batch_size=1, verbose=True):
+        """
+        falta revisar que no inpaint step funcione, el sort tambien y la unnormalizacion tambien. evaluar todo aca.
+        """
         
         ## falta
         conditions = {k: self.preprocess_fn(v) for k, v in conditions.items()}
@@ -41,31 +79,30 @@ class Policy:
 
         no_inpaint_step=conditions.shape #ver esto.
         #falta
+
+        
         ## run reverse diffusion process
         trajectories = self.diffusion_model(conditions, verbose=verbose, **self.sample_kwargs) # inpainting... agregar algo para sample by inpainting
 
-
-        trajectories , values = sort_by_values(trajectories) #sort by sampled returns. quizas esta no es la mejor metrica? puede ser que sea inconsistente? 
-
-
-        #trajectories = utils.to_np(trajectories)
-
-        rewards = trajectories[:, :, -1]
-        rewards = self.dataset.normalizer.unnormalize_torch(rewards, 'rewards') # normalizar solo acciones y observaciones ... aunque tambien se podrian normalizar las rewards... probar esto... 
+        normed_rewards = trajectories[:, :, -1]
+        rewards = self.dataset.normalizer.unnormalize_torch(normed_rewards, 'rewards') # normalizar solo acciones y observaciones ... aunque tambien se podrian normalizar las rewards... probar esto... 
         
-        # hacer todo esto en torch
-
         ## extract action [ batch_size x horizon x transition_dim + 1 ] transition_dim=actions+observations+rewards / + goal
-        actions = trajectories[:, :, :self.action_dim]
-        actions = self.dataset.normalizer.unnormalize_torch(actions, 'actions') # normalizar solo acciones y observaciones ... aunque tambien se podrian normalizar las rewards... probar esto... 
-
-        ## extract first action
-        action = actions[0, 0]
+        normed_actions = trajectories[:, :, :self.action_dim]
+        actions = self.dataset.normalizer.unnormalize_torch(normed_actions, 'actions') # normalizar solo acciones y observaciones ... aunque tambien se podrian normalizar las rewards... probar esto... 
 
         normed_observations = trajectories[:, :, self.action_dim:-1] 
         observations = self.dataset.normalizer.unnormalize_torch(normed_observations, 'observations')
-        trajectories = Trajectories(actions, observations, values) # generalizar aca ...
-        return action, trajectories
+
+        #IMPIORTANTE: CORTAR ACA POR no inpaint step ...
+        actions_sorted, observations_sorted, rewards_sorted, values = sort_by_values(actions, observations, rewards, no_inpaint_step, gamma=self.gamma) #sort by sampled returns. quizas esta no es la mejor metrica? puede ser que sea inconsistente? 
+
+        ## extract first action
+        action = actions_sorted[0, 0]
+
+        trajectories = Trajectories(actions_sorted, observations_sorted, rewards_sorted) # generalizar aca ...
+
+        return action, trajectories, values
 
     @property
     def device(self):
