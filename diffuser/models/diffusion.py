98f052eaee316d34b37b78927edb9a308de69cd8
augusto <augustotaglemontes@gmail.com>
2024-08-27 19:55:59 -0400

    added task temporal u net

--- diffuser/models/diffusion.py
+++ diffuser/models/diffusion.py
@@ -86,35 +86,6 @@ class GaussianDiffusion(nn.Module):
 
         self.bernoulli_dist = torch.distributions.Bernoulli(p_mask)
 
-    def get_random_mask(self):
-        #TODO samplear de una uniforme y mitad mitad... o quizas menos...
-        raise NotImplementedError
-
- 
-    def get_mask_loss_weights(self,K_step_mask):
-        '''
-            sets loss coefficients for masked trajectory
-            
-            K_step_mask: (B,)
-        returns: loss_weights: (B,H,T)
-        '''
-        batch_size=len(K_step_mask)
-        dim_weights=torch.ones(batch_size, self.horizon, self.transition_dim,dtype=torch.float32)
-        #dim_weights = torch.ones(self.transition_dim, dtype=torch.float32)
-
-        ## decay loss with trajectory timestep: discount**t
-        #discounts = self.loss_discount ** torch.arange(self.horizon-K, dtype=torch.float) #TODO: horizon es todo el horizonte y h-K es lo que hay q predecir... 
-        #discounts = discounts / discounts.mean() # TODO: para usar los discount podemos calcularlos antes... y sumarlos al mask 
-
-        #loss_weights = torch.einsum('h-k,t->(h-k)t', discounts, dim_weights)
-
-        loss_weights[:,K_step_mask, :self.action_dim] = self.action_weight
-        loss_weights[:,K_step_mask, self.action_dim:self.action_dim+self.observation_dim] = 0 # because the initial state is given 
-        loss_weights[:,:K_step_mask, :] = 0 # mask behind
-
-        return loss_weights
-
-
 
     #------------------------------------------ sampling ------------------------------------------#
 
@@ -257,3 +228,227 @@ class GaussianDiffusion(nn.Module):
     def forward(self, past_K_history, *args, **kwargs):
         return self.conditional_sample(past_K_history, *args, **kwargs) # TODO: repaint sampling... 
 
+
+
+
+
+"""
+ def get_mask_loss_weights(self,K_step_mask):
+        '''
+            sets loss coefficients for masked trajectory
+            
+            K_step_mask: (B,)
+        returns: loss_weights: (B,H,T)
+        '''
+        batch_size=len(K_step_mask)
+        dim_weights=torch.ones(batch_size, self.horizon, self.transition_dim,dtype=torch.float32)
+        #dim_weights = torch.ones(self.transition_dim, dtype=torch.float32)
+
+        ## decay loss with trajectory timestep: discount**t
+        #discounts = self.loss_discount ** torch.arange(self.horizon-K, dtype=torch.float) #TODO: horizon es todo el horizonte y h-K es lo que hay q predecir... 
+        #discounts = discounts / discounts.mean() # TODO: para usar los discount podemos calcularlos antes... y sumarlos al mask 
+
+        #loss_weights = torch.einsum('h-k,t->(h-k)t', discounts, dim_weights)
+
+        loss_weights[:,K_step_mask, :self.action_dim] = self.action_weight
+        loss_weights[:,K_step_mask, self.action_dim:self.action_dim+self.observation_dim] = 0 # because the initial state is given 
+        loss_weights[:,:K_step_mask, :] = 0 # mask behind
+
+        return loss_weights
+
+"""
+
+
+
+
+"""
+    def get_random_mask(self):
+        #TODO samplear de una uniforme y mitad mitad... o quizas menos...
+        raise NotImplementedError
+
+"""
+
+
+
+class GaussianDiffusion_task(nn.Module):
+    def __init__(self, model, horizon, observation_dim, action_dim, n_timesteps=1000,
+        loss_type='l2', clip_denoised=True,
+        action_weight=1.0, loss_discount=1.0):
+        super().__init__()
+        self.horizon = horizon
+        self.observation_dim = observation_dim
+        self.action_dim = action_dim
+        self.transition_dim = observation_dim + action_dim+1 # (S+A+R)
+        self.model = model
+        self.action_weight=action_weight
+        self.loss_discount=loss_discount
+
+        betas = cosine_beta_schedule(n_timesteps)
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = torch.cat([torch.ones(1), alphas_cumprod[:-1]])
+
+        self.n_timesteps = int(n_timesteps)
+        self.clip_denoised = clip_denoised
+
+        self.register_buffer('betas', betas)
+        self.register_buffer('alphas_cumprod', alphas_cumprod)
+        self.register_buffer('alphas_cumprod_prev', alphas_cumprod_prev)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod))
+        self.register_buffer('sqrt_one_minus_alphas_cumprod', torch.sqrt(1. - alphas_cumprod))
+        self.register_buffer('log_one_minus_alphas_cumprod', torch.log(1. - alphas_cumprod))
+        self.register_buffer('sqrt_recip_alphas_cumprod', torch.sqrt(1. / alphas_cumprod))
+        self.register_buffer('sqrt_recipm1_alphas_cumprod', torch.sqrt(1. / alphas_cumprod - 1))
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+        self.register_buffer('posterior_variance', posterior_variance)
+
+        ## log calculation clipped because the posterior variance
+        ## is 0 at the beginning of the diffusion chain
+        self.register_buffer('posterior_log_variance_clipped',
+            torch.log(torch.clamp(posterior_variance, min=1e-20)))
+        self.register_buffer('posterior_mean_coef1',
+            betas * np.sqrt(alphas_cumprod_prev) / (1. - alphas_cumprod))
+        self.register_buffer('posterior_mean_coef2',
+            (1. - alphas_cumprod_prev) * np.sqrt(alphas) / (1. - alphas_cumprod))
+
+        ## get loss coefficients and initialize objective
+        self.loss_fn = Losses[loss_type](self.action_dim)
+
+    #------------------------------------------ sampling ------------------------------------------#
+
+    def predict_start_from_noise(self, x_t, t, noise):
+        '''
+            
+        '''
+        return (
+            extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t -
+            extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * noise
+        )
+
+    def q_posterior(self, x_start, x_t, t):
+        posterior_mean = (
+            extract(self.posterior_mean_coef1, t, x_t.shape) * x_start +
+            extract(self.posterior_mean_coef2, t, x_t.shape) * x_t
+        )
+        posterior_variance = extract(self.posterior_variance, t, x_t.shape)
+        posterior_log_variance_clipped = extract(self.posterior_log_variance_clipped, t, x_t.shape)
+        return posterior_mean, posterior_variance, posterior_log_variance_clipped
+
+    
+
+
+    def p_mean_variance(self,x):
+
+        epsilon = self.model(x=x, t=t, force_dropout=True)
+
+        t = t.detach().to(torch.int64)
+        x_recon = self.predict_start_from_noise(x, t=t, noise=epsilon)
+
+        if self.clip_denoised:
+            x_recon.clamp_(-1., 1.)
+        else:
+            assert RuntimeError()
+
+        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(
+                x_start=x_recon, x_t=x, t=t)
+        return model_mean, posterior_variance, posterior_log_variance
+
+
+    
+    @torch.no_grad()
+    def p_sample(self, x, past_K_history,K_step, t, returns=None): # Falta REpaint sampling TODO
+        b, *_, device = *x.shape, x.device
+        model_mean, _, model_log_variance = self.p_mean_variance(x=x, past_K_history=past_K_history, K_step=K_step, t=t, returns=returns)
+        noise = 0.5*torch.randn_like(x)
+        # no noise when t == 0
+        nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x.shape) - 1)))
+        return model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
+
+    @torch.no_grad()
+    def p_sample_loop(self, shape, past_K_history, verbose=True, return_chain=False, sample_fn=default_sample_fn, **sample_kwargs):
+        """ Classical DDPM (check this) sampling algorithm with sample_fn incorporated and conditioning
+        
+        """
+        device = self.betas.device
+
+        batch_size = shape[0]
+        x = torch.randn(shape, device=device) # TODO:  en dd usan 0.5*torch.randn(shape, device=device) y no en mtdiff
+        #x = apply_conditioning(x, cond, self.action_dim) # apply conditioning to first sample 
+
+        chain = [x] if return_chain else None
+        K_step=past_K_history.shape[1] # ver esto... TODO
+        progress = utils.Progress(self.n_timesteps) if verbose else utils.Silent()
+        for i in reversed(range(0, self.n_timesteps)):
+            t = make_timesteps(batch_size, i, device)
+            x = self.p_sample(self, x, past_K_history,K_step, t, **sample_kwargs) # sample with conditions
+        #    x = apply_conditioning(x, cond, self.action_dim) # apply conditioning again # TODO: cambiar aca por el mask y todo y usar repaint sampling... 
+
+            progress.update({'t': i})
+            if return_chain: chain.append(x)
+
+        progress.stamp() # en otros usar .close()
+        # cambio aqui el sort b yvalues. es lo mismo solo q lo uso en la policy para tener mas control de lo que pasa y hacer um algoritmo mas general... 
+        if return_chain: chain = torch.stack(chain, dim=1)
+        return Sample(x,  chain)
+
+    @torch.no_grad()
+    def conditional_sample(self, past_K_history, horizon=None, **sample_kwargs):
+        '''
+            conditions : [ (time, state), ... ]
+        '''
+        device = self.betas.device
+        batch_size = len(past_K_history[0]) # TODO ver esto...  asume que el past k history esta batchified
+        horizon = horizon or self.horizon
+        shape = (batch_size, horizon, self.transition_dim)
+
+        return self.p_sample_loop(shape, past_K_history, **sample_kwargs) #   TODO add returns conditioning... 
+
+    #------------------------------------------ training ------------------------------------------#
+
+    def q_sample(self, x_start, t, noise=None):
+        if noise is None:
+            noise = torch.randn_like(x_start)
+
+        sample = (
+            extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start +
+            extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape) * noise
+        )
+
+        return sample
+
+    def p_losses(self, x_start,returns,K_step_mask, t):
+        #TODO: OJO QUE ACA PARA MTDIFF usan unos einops, ver esto... 
+        noise = torch.randn_like(x_start)
+
+        x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+        #x_noisy = apply_conditioning(x_noisy, cond, self.action_dim)
+
+        pred_epsilon = self.model(x_noisy, returns, t, K=K_step_mask,use_dropout=True) # TODO: maybe add task and actual K step. ver modelo tambien
+
+        assert noise.shape == pred_epsilon.shape
+        loss_weights=self.get_mask_loss_weights(K_step_mask) # (B,)-> (B,H,T)
+        loss, info = self.loss_fn(pred_epsilon, noise,loss_weights) # TODO: falta ver la funcion de loss... 
+
+        return loss, info
+
+
+    def loss(self, x, returns, *args): # maybe add task and actual K step.
+
+        batch_size = len(x) # TODO aca mtdiff hace algo con einops...
+        t = torch.randint(0, self.n_timesteps, (batch_size,), device=x.device).long()
+
+        K_step_mask = torch.randint(0, self.horizon, (batch_size,), device=x.device).long() # [0,H-1] #TODO test horizon... 
+        samples = self.bernoulli_dist.sample((batch_size,))
+        K_step_mask=samples*K_step_mask  # mask with certain probability
+        # loss weights = torch.ones[:K_step_mask]=0
+
+        return self.p_losses(x, returns, K_step_mask.long(), t) # TODO: maybe add task and actual K step.
+
+
+
+    def forward(self, past_K_history, *args, **kwargs):
+        return self.conditional_sample(past_K_history, *args, **kwargs) # TODO: repaint sampling... 
--- diffuser/models/temporal.py
+++ diffuser/models/temporal.py
@@ -192,3 +192,123 @@ class TemporalUnet(nn.Module):
             return grad[0]
         else:
             return x
+
+
+
+
+class TemporalUnet_task(nn.Module):
+    """
+    Task inference specific temporal u net, with no conditioning.
+    """ 
+    #TODO use cross attention... 
+
+    def __init__(
+        self,
+        horizon,
+        transition_dim,
+        dim=32,
+        dim_mults=(1, 2, 4, 8),
+        attention=False,
+        calc_energy=False
+    ):
+        super().__init__()
+
+        dims = [transition_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+        print(f'[ models/temporal ] Channel dimensions: {in_out}')
+
+        self.calc_energy = calc_energy
+    
+        embed_dim = dim
+
+        self.time_mlp = nn.Sequential(
+            SinusoidalPosEmb(dim),
+            nn.Linear(dim, dim * 4),
+            nn.Mish(),
+            nn.Linear(dim * 4, dim),
+        )
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        print(in_out)
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(nn.ModuleList([
+                ResidualTemporalBlock(dim_in, dim_out, embed_dim=embed_dim, horizon=horizon),
+                ResidualTemporalBlock(dim_out, dim_out, embed_dim=embed_dim, horizon=horizon),
+                Residual(PreNorm(dim_out, LinearAttention(dim_out))) if attention else nn.Identity(), # dd le saca los resifuales  # maybe use cross attention... 
+                Downsample1d(dim_out) if not is_last else nn.Identity()
+            ]))
+
+            if not is_last:
+                horizon = horizon // 2
+
+        mid_dim = dims[-1]
+        self.mid_block1 = ResidualTemporalBlock(mid_dim, mid_dim, embed_dim=embed_dim, horizon=horizon)
+        self.mid_attn = Residual(PreNorm(mid_dim, LinearAttention(mid_dim))) if attention else nn.Identity() # dd le saca esto
+        self.mid_block2 = ResidualTemporalBlock(mid_dim, mid_dim, embed_dim=embed_dim, horizon=horizon)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.ups.append(nn.ModuleList([
+                ResidualTemporalBlock(dim_out * 2, dim_in, embed_dim=embed_dim, horizon=horizon),
+                ResidualTemporalBlock(dim_in, dim_in, embed_dim=embed_dim, horizon=horizon),
+                Residual(PreNorm(dim_in, LinearAttention(dim_in))) if attention else nn.Identity(), ## dd le saca esto
+                Upsample1d(dim_in) if not is_last else nn.Identity()
+            ]))
+
+            if not is_last:
+                horizon = horizon * 2
+
+        self.final_conv = nn.Sequential(
+            Conv1dBlock(dim, dim, kernel_size=5),
+            nn.Conv1d(dim, transition_dim, 1),
+        )
+
+    def forward(self, x, time):
+        '''
+            x : [ batch x horizon x transition ]
+            returns : [batch x horizon]
+        '''
+        if self.calc_energy:
+            x_inp = x
+
+        x = einops.rearrange(x, 'b h t -> b t h')
+
+        t = self.time_mlp(time)
+
+        h = []
+
+        for resnet, resnet2, attn, downsample in self.downs:
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            h.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        for resnet, resnet2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            x = upsample(x)
+
+        x = self.final_conv(x)
+
+        x = einops.rearrange(x, 'b t h -> b h t')
+
+        if self.calc_energy:
+            # Energy function
+            energy = ((x - x_inp)**2).mean()
+            grad = torch.autograd.grad(outputs=energy, inputs=x_inp, create_graph=True)
+            return grad[0]
+        else:
+            return x
