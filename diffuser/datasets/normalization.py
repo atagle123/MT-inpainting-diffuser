d848522bbaa1eba990eed74dc53bad473fc47e2c
augusto <augustotaglemontes@gmail.com>
2024-09-12 14:25:12 -0300

    refactored and cleaned normalization

--- diffuser/datasets/normalization.py
+++ diffuser/datasets/normalization.py
@@ -1,63 +1,66 @@
 import numpy as np
 import torch
 from diffuser.utils.arrays import atleast_2d
+
 #-----------------------------------------------------------------------------#
 #--------------------------- multi-field normalizer --------------------------#
 #-----------------------------------------------------------------------------#
+# TODO test and maybe join normalize torch with np... view time... 
 
 class Normalizer:
-    def __init__(self,dataset,normed_keys,use_padding,max_len):
+    def __init__(self,
+                 dataset,
+                 normed_keys
+                 ):
         self.dataset=dataset
-        self.use_padding=use_padding
-        self.max_len=max_len
         self.make_params(normed_keys=normed_keys)
+
 
     def make_params(self,normed_keys=['observations', 'actions']):
 
-        dict_of_list = {key: [] for key in normed_keys} # cambiar nombre
+        keys_attribute_dict= {key: [] for key in normed_keys} # cambiar nombre
         
         ### Calculate means and std and normalize dataset ###
         for ep_id, dict in self.dataset.items():
             for key,attribute in dict.items():
                 attribute=atleast_2d(attribute)
-               # if self.use_padding:
-    
-                  #  attribute=pad(attribute,max_len=self.max_len)
+
                 if key in normed_keys:
-                    dict_of_list[key].append(attribute)
+                    keys_attribute_dict[key].append(attribute)
 
         self.params_dict={}
         for key in normed_keys:
             print(f"Getting normalization params for {key} ...")
-            concat_episodes_key=np.concatenate(dict_of_list[key]) # all episodes concatenated
-            #dict_of_list[key]=dict_of_list[key].astype(np.float32)
+            concat_episodes_key=np.concatenate(keys_attribute_dict[key]) # all episodes concatenated
             mean=concat_episodes_key.mean(axis=0)
-            std=concat_episodes_key.std(axis=0)  #ojo con el dtype
+            std=concat_episodes_key.std(axis=0)  
             min=concat_episodes_key.min(axis=0)
             max=concat_episodes_key.max(axis=0)
             self.params_dict[key]={"mean":mean,"std":std,"min":min,"max":max}
+
+            for key, value in self.params_dict[key].items(): # check dtypes
+                assert value.dtype == np.float32, f"Value for key '{key}' is not of dtype float32"
 
     
     def __call__(self, x, key):
         return self.normalize(x, key)
 
+
     def normalize(self, *args, **kwargs):
         raise NotImplementedError()
+
 
     def unnormalize(self, *args, **kwargs):
         raise NotImplementedError()
     
-
-    
-
 
 class GaussianNormalizer(Normalizer):
     '''
         normalizes to zero mean and unit variance
     '''
 
-    def __init__(self,dataset,normed_keys,use_padding,max_len):
-        super().__init__(dataset,normed_keys,use_padding,max_len)
+    def __init__(self, dataset, normed_keys):
+        super().__init__(dataset, normed_keys)
 
     def normalize(self,unnormed_data, key):
         
@@ -72,7 +75,8 @@ class GaussianNormalizer(Normalizer):
 
         return(unnormed_data)
     
-    def normalize_torch(self,unnormed_data,key,device="cuda:0"):
+    
+    def normalize_torch(self,unnormed_data,key):
         
         torch_mean=torch.from_numpy(self.params_dict[key]["mean"]).to(unnormed_data.device)
         torch_std=torch.from_numpy(self.params_dict[key]["std"]).to(unnormed_data.device)
@@ -84,7 +88,8 @@ class GaussianNormalizer(Normalizer):
 
         return(normed_data)
     
-    def unnormalize_torch(self,normed_data,key,device="cuda:0"):
+    
+    def unnormalize_torch(self,normed_data,key):
         torch_mean=torch.from_numpy(self.params_dict[key]["mean"]).to(normed_data.device)
         torch_std=torch.from_numpy(self.params_dict[key]["std"]).to(normed_data.device)
 
@@ -94,15 +99,13 @@ class GaussianNormalizer(Normalizer):
         unnormed_data=normed_data*torch_std+torch_mean
         return(unnormed_data)
 
-    
-
 
 class LimitsNormalizer(Normalizer):
     '''
         maps [ xmin, xmax ] to [ -1, 1 ]
     '''
-    def __init__(self,dataset,normed_keys,use_padding,max_len):
-        super().__init__(dataset,normed_keys,use_padding,max_len)
+    def __init__(self,dataset,normed_keys):
+        super().__init__(dataset,normed_keys)
     
     def normalize(self,unnormed_data, key): # TODO test... 
         
@@ -120,7 +123,7 @@ class LimitsNormalizer(Normalizer):
         '''
 
         if self.params_dict[key]["max"] > 1 + eps or self.params_dict[key]["min"] < -1 - eps:
-            # print(f'[ datasets/mujoco ] Warning: sample out of range | ({x.min():.4f}, {x.max():.4f})')
+            print(f'Warning: sample out of range | ({self.params_dict[key]["min"].min():.4f}, {self.params_dict[key]["max"].max():.4f})')
             normed_data = np.clip(normed_data, -1, 1)
 
         ## [ -1, 1 ] --> [ 0, 1 ]
@@ -128,6 +131,7 @@ class LimitsNormalizer(Normalizer):
 
         return normed_data * (self.params_dict[key]["max"] - self.params_dict[key]["min"]) + self.params_dict[key]["min"]
     
+
     def normalize_torch(self,unnormed_data,key):
 
         torch_min=torch.from_numpy(self.params_dict[key]["min"]).to(unnormed_data.device)
@@ -152,8 +156,8 @@ class LimitsNormalizer(Normalizer):
         assert torch_min.shape==torch_max.shape
         assert torch_max.shape==normed_data[0,0,:].shape
 
-        if torch_max > 1 + eps or torch_min < -1 - eps: # ver si se puede hacer comparacion... 
-            # print(f'[ datasets/mujoco ] Warning: sample out of range | ({x.min():.4f}, {x.max():.4f})')
+        if (torch_max > 1 + eps).any() or (torch_min < -1 - eps).any(): # ver si se puede hacer comparacion... 
+            print(f'Warning: sample out of range | ({torch_min.min():.4f}, {torch_max.max():.4f})')
             normed_data = torch.clip(normed_data, -1, 1)
 
         ## [ -1, 1 ] --> [ 0, 1 ]
