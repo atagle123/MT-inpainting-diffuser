9c23f882d5500ed134a1589032d68cbae5f88735
augusto <augustotaglemontes@gmail.com>
2024-09-12 20:30:23 -0300

    added no conditioning temporal u net

--- diffuser/models/temporal.py
+++ diffuser/models/temporal.py
@@ -45,8 +45,105 @@ class ResidualTemporalBlock(nn.Module):
         out = self.blocks[1](out)
         return out + self.residual_conv(x)
 
-
 class TemporalUnet(nn.Module):
+
+    def __init__(
+        self,
+        horizon,
+        transition_dim,
+        dim=32,
+        dim_mults=(1, 2, 4, 8),
+        attention=False,
+    ):
+        super().__init__()
+
+        dims = [transition_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+        print(f'[ models/temporal ] Channel dimensions: {in_out}')
+
+        time_dim = dim
+        self.time_mlp = nn.Sequential(
+            SinusoidalPosEmb(dim),
+            nn.Linear(dim, dim * 4),
+            nn.GELU(),
+            nn.Linear(dim * 4, dim),
+        )
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        print(in_out)
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(nn.ModuleList([
+                ResidualTemporalBlock(dim_in, dim_out, embed_dim=time_dim, horizon=horizon),
+                ResidualTemporalBlock(dim_out, dim_out, embed_dim=time_dim, horizon=horizon),
+                Residual(PreNorm(dim_out, LinearAttention(dim_out))) if attention else nn.Identity(),
+                Downsample1d(dim_out) if not is_last else nn.Identity()
+            ]))
+
+            if not is_last:
+                horizon = horizon // 2
+
+        mid_dim = dims[-1]
+        self.mid_block1 = ResidualTemporalBlock(mid_dim, mid_dim, embed_dim=time_dim, horizon=horizon)
+        self.mid_attn = Residual(PreNorm(mid_dim, LinearAttention(mid_dim))) if attention else nn.Identity()
+        self.mid_block2 = ResidualTemporalBlock(mid_dim, mid_dim, embed_dim=time_dim, horizon=horizon)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.ups.append(nn.ModuleList([
+                ResidualTemporalBlock(dim_out * 2, dim_in, embed_dim=time_dim, horizon=horizon),
+                ResidualTemporalBlock(dim_in, dim_in, embed_dim=time_dim, horizon=horizon),
+                Residual(PreNorm(dim_in, LinearAttention(dim_in))) if attention else nn.Identity(),
+                Upsample1d(dim_in) if not is_last else nn.Identity()
+            ]))
+
+            if not is_last:
+                horizon = horizon * 2
+
+        self.final_conv = nn.Sequential(
+            Conv1dBlock(dim, dim, kernel_size=5),
+            nn.Conv1d(dim, transition_dim, 1),
+        )
+
+    def forward(self, x, cond, time):
+        '''
+            x : [ batch x horizon x transition ]
+        '''
+
+        x = einops.rearrange(x, 'b h t -> b t h')
+
+        t = self.time_mlp(time)
+        h = []
+
+        for resnet, resnet2, attn, downsample in self.downs:
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            h.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        for resnet, resnet2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = resnet(x, t)
+            x = resnet2(x, t)
+            x = attn(x)
+            x = upsample(x)
+
+        x = self.final_conv(x)
+
+        x = einops.rearrange(x, 'b t h -> b h t')
+        return x
+    
+class TemporalUnet_returns(nn.Module):
 
     def __init__(
         self,
@@ -210,7 +307,7 @@ class TemporalUnet_task_rtg(nn.Module): # continum task...
         dim_mults=(1, 2, 4, 8),
         attention=True,
         calc_energy=True,
-        task_dims=2
+        task_dim=2
     ):
         super().__init__()
 
@@ -244,7 +341,7 @@ class TemporalUnet_task_rtg(nn.Module): # continum task...
                 )
         
         self.task_mlp = nn.Sequential(
-                    nn.Linear(task_dims, dim),
+                    nn.Linear(task_dim, dim),
                     act_fn,
                     nn.Linear(dim, dim * 4),
                     act_fn,
@@ -260,11 +357,11 @@ class TemporalUnet_task_rtg(nn.Module): # continum task...
                 )
         
         self.decoder=nn.Sequential(
-                    nn.Linear(task_dims, dim),
+                    nn.Linear(task_dim, dim),
                     act_fn,
                     nn.Linear(dim, dim * 4),
                     act_fn,
-                    nn.Linear(dim * 4, task_dims+1), # returns task,rtg... 
+                    nn.Linear(dim * 4, task_dim+1), # returns task,rtg... 
                 )
 
 
@@ -519,3 +616,6 @@ class TemporalUnet_mode(nn.Module): # continum task...
             return grad[0]
         else:
             return x
+        
+
+    
